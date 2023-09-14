d700c85c20494669f4e2abd6e2c14aedb29fb0d9
Evertton <evertton.martins@gmail.com>
2023-09-14 09:20:21 -0300

    novo campos

--- src/containers/ModalCreateProduct/index.js
+++ src/containers/ModalCreateProduct/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { Modal } from "../../components/Modal/Modal";
-import { Button, Form } from "react-bootstrap";
+import { Button, Container, Form } from "react-bootstrap";
 import { useAppContext } from "../../storage/AppContext";
 import { saveProductsAction } from "../../actions/productActions";
 import {
@@ -34,9 +34,9 @@ export const ModalCreateProduct = ({ open }) => {
     stock: "",
     image: "",
     items: [],
-    blockedDays: [false,false,false,false,false,false,false],
+    blockedDays: [false, false, false, false, false, false, false],
     startDate: new Date(),
-    endDate: new Date()
+    endDate: new Date(),
   });
   const [productData, setProductData] = useState(initialProduct.current); // o negocio que vai mandar os dados
 
@@ -92,29 +92,27 @@ export const ModalCreateProduct = ({ open }) => {
       [field]: field === "image" ? e.target.files[0] : e.target.value,
     }));
 
-    const handleHours = (value, field) =>
+  const handleHours = (value, field) =>
     setProductData((prevState) => ({
       ...prevState,
-      [field]: value
+      [field]: value,
     }));
 
   const handleItemClick = () =>
     dispatch(openModalSaveItemsAction(productData?.items));
 
-    const handleBlockedDays = (index) =>
+  const handleBlockedDays = (index) =>
     setProductData((prevState) => {
-    let blockedDays = [...prevState.blockedDays];
-    blockedDays[index] = blockedDays[index] ? false : true;
-    return {
-      ...prevState,
-      blockedDays: blockedDays,
-    }
+      let blockedDays = [...prevState.blockedDays];
+      blockedDays[index] = blockedDays[index] ? false : true;
+      return {
+        ...prevState,
+        blockedDays: blockedDays,
+      };
     });
-
 
   return (
     <Modal
-      // title={(state?.activeProduct?.id ? "Editar" : "Criar") + " Produto"}
       open={open}
       controls={[
         {
@@ -146,129 +144,133 @@ export const ModalCreateProduct = ({ open }) => {
               <Row>
                 <Col md={7}>
                   <Row>
-                  <Form.Label>Nome do serviço</Form.Label>
-                  <Form.Control
-                    type="text"
-                    required
-                    placeholder=""
-                    value={productData?.title}
-                    onChange={(e) => handleChange(e, "title")}
-                  />
+                    <Form.Label>Nome do serviço</Form.Label>
+                    <Form.Control
+                      type="text"
+                      required
+                      placeholder=""
+                      value={productData?.title}
+                      onChange={(e) => handleChange(e, "title")}
+                    />
                   </Row>
-                  <br/>
+                  <br />
                   <Row>
-                  <Form.Label>descrição</Form.Label>
-                  <Form.Control
-                    as="textarea"
-                    rows={10}
-                    required
-                    value={productData?.description}
-                    onChange={(e) => handleChange(e, "description")}
-                  />
+                    <Form.Label>descrição</Form.Label>
+                    <Form.Control
+                      as="textarea"
+                      rows={10}
+                      required
+                      value={productData?.description}
+                      onChange={(e) => handleChange(e, "description")}
+                    />
                   </Row>
                 </Col>
                 <Col md={5}>
                   <Row>
-                  <Form.Label>preço sem frete</Form.Label>
-                  <Form.Control
-                    type="number"
-                    required
-                    value={productData?.price}
-                    onChange={(e) => handleChange(e, "price")}
-                  />
+                    <Form.Label>preço sem frete</Form.Label>
+                    <Form.Control
+                      type="number"
+                      required
+                      value={productData?.price}
+                      onChange={(e) => handleChange(e, "price")}
+                    />
                   </Row>
-                  <br/>
+                  <br />
                   <Row>
-                  <Form.Label>preço com frete Recife</Form.Label>
-                  <Form.Control
-                    type="number"
-                    required
-                    value={productData?.priceRecife}
-                    onChange={(e) => handleChange(e, "priceRecife")}
-                  />
+                    <Form.Label>preço com frete Recife</Form.Label>
+                    <Form.Control
+                      type="number"
+                      required
+                      value={productData?.priceRecife}
+                      onChange={(e) => handleChange(e, "priceRecife")}
+                    />
                   </Row>
-                  <br/>
+                  <br />
                   <Row>
-                  <Form.Label>preço com frete RMR</Form.Label>
-                  <Form.Control
-                    type="number"
-                    required
-                    value={productData?.priceRMR}
-                    onChange={(e) => handleChange(e, "priceRMR")}
-                  />
+                    <Form.Label>preço com frete RMR</Form.Label>
+                    <Form.Control
+                      type="number"
+                      required
+                      value={productData?.priceRMR}
+                      onChange={(e) => handleChange(e, "priceRMR")}
+                    />
                   </Row>
-                  <br/>
-                  {/* <Form.Control
-                    type="number"
-                    required
-                    placeholder="Quantidade em Estoque"
-                    value={productData?.stock}
-                    onChange={(e) => handleChange(e, "stock")}
-                  /> */}
+                  <br />
                   <Row>
-                  <Form.Label>número de pessoas</Form.Label>
+                    <Form.Label>número de pessoas</Form.Label>
                     <Col>
-                    <Form.Label>mínimo</Form.Label>
-                  <Form.Control
-                    type="number"
-                    required
-                    placeholder=""
-                    value={productData?.minPeople}
-                    onChange={(e) => handleChange(e, "minPeople")}
-                  />
-                  </Col>
-                  <Col>
-                  <Form.Label>máximo</Form.Label>
-                  <Form.Control
-                    type="number"
-                    required
-                    placeholder=""
-                    value={productData?.maxPeople}
-                    onChange={(e) => handleChange(e, "maxPeople")}
-                  />
-                  </Col>
+                      <Form.Label>mínimo</Form.Label>
+                      <Form.Control
+                        type="number"
+                        required
+                        placeholder=""
+                        value={productData?.minPeople}
+                        onChange={(e) => handleChange(e, "minPeople")}
+                      />
+                    </Col>
+                    <Col>
+                      <Form.Label>máximo</Form.Label>
+                      <Form.Control
+                        type="number"
+                        required
+                        placeholder=""
+                        value={productData?.maxPeople}
+                        onChange={(e) => handleChange(e, "maxPeople")}
+                      />
+                    </Col>
                   </Row>
-                  {/* <Button variant="light"
-                        style={{width:'100%',textAlign:'start',
-                        border: '1px solid #ced4da',
-                        borderRadius: '0.375rem'}}
-                        onClick={handleItemClick}>
-                          {productData?.items.length 
-                          ? productData?.items?.map(ele => <p key={ele.id}>{ele.total} - {ele.title}</p>) 
-                          : "Selecionar Itens"}
-                   </Button> */}
                 </Col>
               </Row>
               <Row>
-                <p>dias disponiveis horário disponiveis</p>
-                <MultiRatio
-                onClick={handleBlockedDays}
-                elements={productData?.blockedDays}
-                controls={[{
-                  label:'D'
-                },{
-                  label:'S'
-                },{
-                  label:'T'
-                },{
-                  label:'Q'
-                },{
-                  label:'Q'
-                },{
-                  label:'S'
-                },{
-                  label:'S'
-                },]}
-                />
-                <InputTime
-                startDate={productData?.startDate}
-                setStartDate={(e) => handleHours(e, "startDate")}
-                />
-
-                <InputTime
-                startDate={productData?.endDate}
-                setStartDate={(e) => handleHours(e, "endDate")}
-                />
+                <Col md={7}>
+                  <p>dias disponiveis</p>
+                  <MultiRatio
+                    onClick={handleBlockedDays}
+                    elements={productData?.blockedDays}
+                    controls={[
+                      {
+                        label: "D",
+                      },
+                      {
+                        label: "S",
+                      },
+                      {
+                        label: "T",
+                      },
+                      {
+                        label: "Q",
+                      },
+                      {
+                        label: "Q",
+                      },
+                      {
+                        label: "S",
+                      },
+                      {
+                        label: "S",
+                      },
+                    ]}
+                  />
+                </Col>
+                <Col md={5}>
+                  <p>horário disponiveis</p>
+                  <Row>
+                    <Col>
+                      <InputTime
+                        className="col-12"
+                        startDate={productData?.startDate}
+                        setStartDate={(e) => handleHours(e, "startDate")}
+                      />
+                    </Col>
+                    <Col>
+                      <InputTime
+                        className="col-12"
+                        startDate={productData?.endDate}
+                        setStartDate={(e) => handleHours(e, "endDate")}
+                      />
+                    </Col>
+                  </Row>
+                </Col>
               </Row>
             </Col>
           </Row>
